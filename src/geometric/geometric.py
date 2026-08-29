01e0100f706d8c679d42534fe0bc4ac35338ec83
sophie <pspenagos07@ucatolica.edu.co>
2026-08-28 22:26:01 -0500

    area trapecio

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -110,7 +110,7 @@ class Geometria:
         Returns:
             float: Área del trapecio
         """
-        pass
+        return ((base_mayor + base_menor) / 2) * altura
     
     def area_rombo(self, diagonal_mayor, diagonal_menor):
         """
