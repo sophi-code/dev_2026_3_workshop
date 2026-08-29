0aa63d6ca97b0e28faec5075a769643b54ccf007
sophie <pspenagos07@ucatolica.edu.co>
2026-08-28 22:30:06 -0500

    area pentagono

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -136,7 +136,7 @@ class Geometria:
         Returns:
             float: Área del pentágono regular
         """
-        pass
+        return (5 * lado * apotema) / 2
     
     def perimetro_pentagono_regular(self, lado):
         """
