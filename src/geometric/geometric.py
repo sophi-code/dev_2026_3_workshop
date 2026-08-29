4bf21c1d3a01f594438c48666d497f2886aae672
sophie <pspenagos07@ucatolica.edu.co>
2026-08-28 22:10:41 -0500

    perimetro rectangulo

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -28,8 +28,7 @@ class Geometria:
         Returns:
             float: Perímetro del rectángulo
         """
-        pass
-    
+        return 2*(base+altura)
     def area_circulo(self, radio):
         """
         Calcula el área de un círculo.
