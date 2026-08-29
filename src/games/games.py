ba0e83233a6e5011c061dbac80fdad3c3190b842
sophie <pspenagos07@ucatolica.edu.co>
2026-08-28 21:59:38 -0500

    adivinar numero pista

--- src/games/games.py
+++ src/games/games.py
@@ -39,7 +39,12 @@ class Games:
         Returns:
             str: "correcto", "muy alto" o "muy bajo"
         """
-        pass
+        if intento == numero_secreto:
+            return "correcto"
+        elif intento > numero_secreto:
+            return "muy alto"
+        else:
+            return "muy bajo"
     
     def ta_te_ti_ganador(self, tablero):
         """
