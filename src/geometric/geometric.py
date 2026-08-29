e598cb1020993a2e0e410c64879dc3c790aa96df
sophie <pspenagos07@ucatolica.edu.co>
2026-08-28 22:53:56 -0500

    distancia entre puntos

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -262,7 +262,7 @@ class Geometria:
         Returns:
             float: Distancia entre los dos puntos
         """
-        pass
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
     
     def punto_medio(self, x1, y1, x2, y2):
         """
