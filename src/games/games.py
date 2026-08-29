2d0fd2e30bb27c7e353e884a4c6ae23ca3e49243
sophie <pspenagos07@ucatolica.edu.co>
2026-08-28 22:03:43 -0500

    tic tac toe

--- src/games/games.py
+++ src/games/games.py
@@ -61,7 +61,28 @@ class Games:
              ["O", "O", " "],
              [" ", " ", " "]] -> "X"
         """
-        pass
+    
+        for fila in tablero:
+            if fila[0] == fila[1] == fila[2] != " ":
+                return fila[0]
+        
+        
+        for col in range(3):
+            if tablero[0][col] == tablero[1][col] == tablero[2][col] != " ":
+                return tablero[0][col]
+        
+        
+        if tablero[0][0] == tablero[1][1] == tablero[2][2] != " ":
+            return tablero[0][0]
+        if tablero[0][2] == tablero[1][1] == tablero[2][0] != " ":
+            return tablero[0][2]
+        
+        
+        for fila in tablero:
+            if " " in fila:
+                return "continua"
+        
+        return "empate"
     
     def generar_combinacion_mastermind(self, longitud, colores_disponibles):
         """
