3a245cee23247aa651f6977d230af8a479e0586a
sophie <pspenagos07@ucatolica.edu.co>
2026-08-28 22:44:18 -0500

    area superficie esfera

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -221,7 +221,7 @@ class Geometria:
         Returns:
             float: Área de la superficie de la esfera
         """
-        pass
+        return 4 * 3.14159 * (radio ** 2)
     
     def volumen_cilindro(self, radio, altura):
         """
