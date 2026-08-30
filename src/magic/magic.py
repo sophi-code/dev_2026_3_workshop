2581910bff540db52d4a5fb480920eb2edf39765
sophie <pspenagos07@ucatolica.edu.co>
2026-08-29 20:51:45 -0500

    triangulo pascal

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -110,7 +110,15 @@ class Magic:
         Returns:
             list: Lista de listas que representa el triángulo de Pascal
         """
-        pass
+        if filas < 1:
+            return []
+        triangulo = []
+        for i in range(filas):
+            fila = [1] * (i + 1)
+            for j in range(1, i):
+                fila[j] = triangulo[i - 1][j - 1] + triangulo[i - 1][j]
+            triangulo.append(fila)
+        return triangulo
     
     def factorial(self, n):
         """
