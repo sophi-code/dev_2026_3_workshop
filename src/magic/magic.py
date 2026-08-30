05f06822bd4ef7354f351cb7a265d3098cdd0133
sophie <pspenagos07@ucatolica.edu.co>
2026-08-29 23:11:11 -0500

    es cuadrado magico

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -206,4 +206,28 @@ class Magic:
         Returns:
             bool: True si es un cuadrado mágico, False en caso contrario
         """
-        pass
+        n = len(matriz)
+        if any(len(fila) != n for fila in matriz):
+            return False  
+        
+        suma_objetivo = sum(matriz[0])  
+        
+        
+        for fila in matriz:
+            if sum(fila) != suma_objetivo:
+                return False
+        
+        
+        for j in range(n):
+            if sum(matriz[i][j] for i in range(n)) != suma_objetivo:
+                return False
+        
+        
+        if sum(matriz[i][i] for i in range(n)) != suma_objetivo:
+            return False
+        
+        
+        if sum(matriz[i][n - 1 - i] for i in range(n)) != suma_objetivo:
+            return False
+        
+        return True
