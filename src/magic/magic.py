00d34d2de671a1dba33b71364bde25c21756dc95
sophie <pspenagos07@ucatolica.edu.co>
2026-08-29 23:04:10 -0500

    suma de los digitos de un numero

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -179,7 +179,7 @@ class Magic:
         Returns:
             int: La suma de los dígitos de n
         """
-        pass
+        return sum(int(digito) for digito in str(abs(n)))
     
     def es_numero_armstrong(self, n):
         """
