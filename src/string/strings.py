cb12d78fe13247c62bec50b3c870399f57f96d80
sophie <pspenagos07@ucatolica.edu.co>
2026-08-30 22:10:49 -0500

    palindromo

--- src/string/strings.py
+++ src/string/strings.py
@@ -14,7 +14,7 @@ class Strings:
         Returns:
             bool: True si es palíndromo, False en caso contrario
         """
-        pass
+        return texto == texto[::-1] 
     
     def invertir_cadena(self, texto):
         """
