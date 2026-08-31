fe2dae36b5d21939089116cbbf789c4ceb0e2da4
sophie <pspenagos07@ucatolica.edu.co>
2026-08-30 22:18:00 -0500

    contar palabras

--- src/string/strings.py
+++ src/string/strings.py
@@ -88,7 +88,8 @@ class Strings:
         Returns:
             int: Número de palabras en la cadena
         """
-        pass
+        palabras = texto.split()
+        return len(palabras)
     
     def palabras_mayus(self, texto):
         """
