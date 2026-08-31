bf0acd677291525ee00fbb95ebb2b08510161584
sophie <pspenagos07@ucatolica.edu.co>
2026-08-30 22:20:13 -0500

    eliminar espacios duplicados

--- src/string/strings.py
+++ src/string/strings.py
@@ -115,7 +115,8 @@ class Strings:
         Returns:
             str: Cadena sin espacios duplicados
         """
-        pass
+        palabras = texto.split()
+        return " ".join(palabras)
     
     def es_numero_entero(self, texto):
         """
