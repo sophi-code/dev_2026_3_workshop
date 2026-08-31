89af0de5d962670cc7f516d75852668a33d480f7
sophie <pspenagos07@ucatolica.edu.co>
2026-08-30 22:11:45 -0500

    invertir cadena

--- src/string/strings.py
+++ src/string/strings.py
@@ -26,7 +26,10 @@ class Strings:
         Returns:
             str: Cadena invertida
         """
-        pass
+        invertida = ""
+        for caracter in texto:
+            invertida = caracter + invertida
+        return invertida
     
     def contar_vocales(self, texto):
         """
